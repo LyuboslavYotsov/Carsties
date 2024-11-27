525d6c1de97ee20acca9db1bc68ebc3bf7756be0
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2024-11-27 19:55:40 +0200

    DbContext added and Database configured

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -1,5 +1,8 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace AuctionService.Entities;
 
+[Table("Items")]
 public class Item
 {
     public Guid Id { get; set; }
@@ -15,7 +18,7 @@ public class Item
     public int Milage { get; set; }
 
     public string ImageUrl { get; set; }
-    
+
 
     public Auction Auction { get; set; }
 
