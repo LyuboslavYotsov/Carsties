a10d3ea63c992d2129c80652f355e71f1a9adb0b
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2025-01-08 18:14:58 +0200

    Search bar implemented

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,16 +1,13 @@
-'use client'
-
 import React from 'react'
 import {AiOutlineCar} from 'react-icons/ai'
+import Search from './Search'
+import Logo from './Logo'
 
 export default function Navbar() {
   return (
     <header className='sticky top-15 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md'>
-        <div className='flex items-center gap-2 text-3xl font-semibold text-red-500'>
-            <AiOutlineCar size={35} />
-            <div>Carsties Auction</div>
-        </div>
-        <div>Search</div>
+        <Logo />
+        <Search />
         <div>Login</div>
     </header>
   )
