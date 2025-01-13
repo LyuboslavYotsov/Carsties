e2a0ec05aa62c634a3958a1889abcf7657aa1ee9
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2025-01-13 19:31:41 +0200

    Reusable date input created

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -4,6 +4,7 @@ import { Button, TextInput } from 'flowbite-react';
 import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../components/Input';
+import DateInput from '../components/DateInput';
 
 export default function AuctionForm() {
 
@@ -36,8 +37,14 @@ export default function AuctionForm() {
             <div className='grid grid-cols-2 gap-3'>
                 <Input label='Reserve Price (enter 0 if no reserve)'
                     name='reservePrice' control={control} type='number' rules={{ required: 'Reserve Price is required' }} />
-                <Input label='Auction end date/time'
-                    name='auctionEnd' type='date' control={control} rules={{ required: 'Auction end date is required' }} />
+                <DateInput
+                    label='Auction end date/time'
+                    name='auctionEnd'
+                    dateFormat='dd MMMM yyyy h:mm a'
+                    showTimeSelect
+                    control={control} 
+                    rules={{ required: 'Auction end date is required' }}
+                />
             </div>
 
             <div className='flex justify-between'>
