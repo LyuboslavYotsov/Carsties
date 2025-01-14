457a2c36a56dbbda5dcffef5dba28a96860f4abf
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2025-01-14 19:49:24 +0200

    Auction delete implemented

--- frontend/web-app/app/auctions/details/[id]/page.tsx
+++ frontend/web-app/app/auctions/details/[id]/page.tsx
@@ -5,7 +5,8 @@ import CountdownTimer from '../../CountdownTimer';
 import CardImage from '../../CardImage';
 import DetailedSpecs from './DetailedSpecs';
 import { getCurrentUser } from '@/app/actions/authActions';
-import EditButton from '../EditButton';
+import EditButton from './EditButton';
+import DeleteButton from './DeleteButton';
 
 export default async function Details({ params }: { params: Promise<{ id: string }> }) {
 
@@ -16,10 +17,13 @@ export default async function Details({ params }: { params: Promise<{ id: string
     <div>
       <div className='flex justify-between'>
         <div className='flex items-center gap-3'>
-        <Heading title={`${data.make} ${data.model}`} />
-        {user?.username === data.seller && (
-          <EditButton id={data.id} />
-        )}
+          <Heading title={`${data.make} ${data.model}`} />
+          {user?.username === data.seller && (
+            <>
+              <EditButton id={data.id} />
+              <DeleteButton id={data.id} />
+            </>
+          )}
         </div>
         <div className='flex gap-3'>
           <h3 className='text-2xl font-semibold'>Time remaining:</h3>
@@ -29,7 +33,7 @@ export default async function Details({ params }: { params: Promise<{ id: string
 
       <div className='grid grid-cols-2 gap-6 mt-3'>
         <div className='w-full bg-gray-200 relative aspect-[16/10] rounded-lg overflow-hidden'>
-          <CardImage imageUrl={data.imageUrl}/>
+          <CardImage imageUrl={data.imageUrl} />
         </div>
 
         <div className='border-2 rounded-lg p-2 bg-gray-100'>
@@ -38,7 +42,7 @@ export default async function Details({ params }: { params: Promise<{ id: string
       </div>
 
       <div className='mt-3 grid grid-cols-1 rounded-lg'>
-        <DetailedSpecs auction={data}/>
+        <DetailedSpecs auction={data} />
       </div>
 
     </div>
