3aa182f11cc687e4770430453f502fdbd5590251
Lyuboslav Yotsov <114014625+LyuboslavYotsov@users.noreply.github.com>
2025-01-20 17:39:24 +0200

    Notification Service consumers added

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -1,10 +1,14 @@
 using MassTransit;
+using NotificationService.Consumers;
 using NotificationService.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMassTransit(x => 
+builder.Services.AddMassTransit(x =>
 {
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
@@ -16,8 +20,6 @@ builder.Services.AddMassTransit(x =>
 
         cfg.ConfigureEndpoints(context);
     });
-
-    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
 });
 
 builder.Services.AddSignalR();
